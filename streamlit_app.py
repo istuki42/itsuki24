344f68ad54a5613a51d81b857b77809c30903db8
istuki42 <144980821+istuki42@users.noreply.github.com>
2023-09-14 04:44:42 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ st.title('hahahhahahhaha')
 
 # テキスト入力ボックスを作成し、ユーザーからの入力を受け取る
 user_input = st.text_input('あなたの名前を入力してください')
-
+st.text_input=itsuki
 # ボタンを作成し、クリックされたらメッセージを表示
 if st.button('挨拶する'):
     if user_input:  # 名前が入力されているかチェック
